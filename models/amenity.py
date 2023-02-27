1c8647faf74fe96f3a0cdfb9663e2576df9337ea
Sapitorico <renzoyaques@gmail.com>
2023-02-27 16:28:18 -0300

    add a class amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" class Amenity that inherent from BaseModel """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ field to class Amenity """
+    name = ""
