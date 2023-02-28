e224276a91f197fd3f00ae33f5bbf4c9d3e8cb2c
Sapitorico <renzoyaques@gmail.com>
2023-02-27 22:03:45 -0300

    added a new test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,6 +15,64 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(my_model.created_at, datetime)
         self.assertIsInstance(my_model.updated_at, datetime)
 
+    def test_id(self):
+        """ test id """
+        my_model = BaseModel()
+        my_model1 = BaseModel()
+        self.assertNotEqual(my_model.id, my_model1.id)
+
+    def test_created_at(self):
+        """ test created_at """
+        my_model = BaseModel()
+        my_model1 = BaseModel()
+        self.assertNotEqual(my_model.created_at, my_model1.created_at)
+
+    def test_updated_at(self):
+        """ test updated_at """
+        my_model = BaseModel()
+        my_model1 = BaseModel()
+        self.assertNotEqual(my_model.updated_at, my_model1.updated_at)
+
+    def test_str(self):
+        """ test str """
+        my_model = BaseModel()
+        my_model.name = "sapito"
+        my_model.my_number = 21
+        string = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+        self.assertEqual(string, str(my_model))
+
+    def test_to_dict(self):
+        """ test to_dict """
+        my_model = BaseModel()
+        my_model.name = "sapito"
+        my_model.my_number = 21
+        my_model.save()
+        new_dict = my_model.to_dict()
+        self.assertEqual(new_dict['__class__'], 'BaseModel')
+        self.assertEqual(type(new_dict['created_at']), str)
+        self.assertEqual(type(new_dict['updated_at']), str)
+
+    def test_kwargs(self):
+        """ test kwargs """
+        my_model = BaseModel()
+        my_model.name = "sapito"
+        my_model.my_number = 89
+        my_model.save()
+        new_dict = my_model.to_dict()
+        my_model1 = BaseModel(**new_dict)
+        self.assertNotEqual(my_model, my_model1)
+        self.assertEqual(my_model.id, my_model1.id)
+        self.assertEqual(my_model.created_at, my_model1.created_at)
+        self.assertEqual(my_model.updated_at, my_model1.updated_at)
+        self.assertEqual(my_model.name, my_model1.name)
+        self.assertEqual(my_model.my_number, my_model1.my_number)
+
+    def test_save(self):
+        """ test save """
+        my_model = BaseModel()
+        my_model.save()
+        self.assertNotEqual(my_model.created_at, my_model.updated_at)
+
 
 if __name__ == '__main__':
     unittest.main()
