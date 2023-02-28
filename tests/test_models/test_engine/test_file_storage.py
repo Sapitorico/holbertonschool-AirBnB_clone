eff563fb1fefbf6efedf6ded9afc458a5af038d1
Sapitorico <renzoyaques@gmail.com>
2023-02-27 22:53:00 -0300

    added a new test form reload filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 import os
+import json
 
 
 class TestFileStorage(unittest.TestCase):
@@ -50,6 +51,22 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(len(objects), 2)
         for key in objects.keys():
             self.assertTrue(isinstance(objects[key], BaseModel))
+
+    def test_reload_2(self):
+        """ Test reload method """
+        my_model = BaseModel()
+        self.file_storage.new(my_model)
+        self.file_storage.save()
+        with open(self.file_storage._FileStorage__file_path, "r") as f:
+            data = json.load(f)
+        data["BaseModel.1234"] = {"id": "1234", "__class__": "BaseModel", "name": "test"}
+        with open(self.file_storage._FileStorage__file_path, "w") as f:
+            json.dump(data, f)
+        self.file_storage.reload()
+        self.assertIn("BaseModel.1234", self.file_storage.all())
+        obj = self.file_storage.all()["BaseModel.1234"]
+        self.assertEqual(obj.id, "1234")
+        self.assertEqual(obj.name, "test")
 
     def test_file_path(self):
         """ Test __file_path attribute """
