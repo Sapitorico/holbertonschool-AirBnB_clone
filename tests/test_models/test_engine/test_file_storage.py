1f9e3b6746ba0e093f36bbc638bb00bc058de158
Sapitorico <renzoyaques@gmail.com>
2023-02-27 22:32:53 -0300

    update a unittests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,44 +3,44 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-import os.path
-from models import storage
+import os
 
 
 class TestFileStorage(unittest.TestCase):
-    """Tests for the FileStorage class"""
+    """Test cases for the FileStorage class"""
 
     def setUp(self):
-        """Set up the FileStorage instance"""
+        """Set up test environment"""
         self.storage = FileStorage()
 
     def tearDown(self):
-        """Delete the FileStorage instance"""
-        del self.storage
+        """Tear down test environment"""
+        if os.path.isfile(self.storage._FileStorage__file_path):
+            os.remove(self.storage._FileStorage__file_path)
 
     def test_all(self):
         """Test the all method"""
-        self.assertEqual(len(self.storage.all()), 0)
-        model1 = BaseModel()
-        model2 = BaseModel()
-        self.storage.new(model1)
-        self.storage.new(model2)
-        self.assertEqual(len(self.storage.all()), 2)
+        self.assertEqual(self.storage.all(), {})
+
+    def test_new(self):
+        """Test the new method"""
+        model = BaseModel()
+        self.storage.new(model)
+        self.assertEqual(self.storage.all(), {'BaseModel.' + model.id: model})
 
     def test_save(self):
         """Test the save method"""
-        from models import storage
         model = BaseModel()
         self.storage.new(model)
         self.storage.save()
-        self.assertTrue(os.path.isfile("file.json"))
+        self.assertTrue(os.path.isfile(self.storage._FileStorage__file_path))
 
     def test_reload(self):
         """Test the reload method"""
         model = BaseModel()
         self.storage.new(model)
         self.storage.save()
-        FileStorage._FileStorage__objects = {}
+        self.assertIn('BaseModel.' + model.id, self.storage.all())
 
 
 if __name__ == '__main__':
